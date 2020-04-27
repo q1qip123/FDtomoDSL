dbef7da3b4cb850e4a301951bedb312fbd79431e
q1qip123 <q1qip123@gmail.com>
2020-04-27 23:12:01 +0800

    revise and add some production rule

--- tomography.py
+++ tomography.py
@@ -7,8 +7,15 @@ class TomographyBuilder(object):
         return None
     
     def Event(self, event = None):
+        if(event != None):
+            self.event_list.append(event)
+            return self
+                
+        if (self.event_builder != None):
+            self.event_list.append(self.event_builder.getValue())
+            
         self.event_builder = EventBuilder(self)
-        return self.event_builder(event)
+        return self.event_builder
     
     def Station(self, station):
         self.station_list.append(station)
@@ -22,50 +29,56 @@ class TomographyBuilder(object):
         
         
 class EventBuilder(TomographyBuilder):
-    event_list = []
     hypocenter_list = []
     observation_list = []
     hypocenter_builder = None
-    event_builder = None
+    observation_builder = None
     tomography_builder = None
     
     def __init__(self, tomography_builder):
         self.tomography_builder = tomography_builder
         
-    def Event(self, event = None):
-        if(event != None):
-            self.event_list.append(event)
-            return self.tomography_builder
-        
-        if (self.event_builder != None):
-            self.event_list.append(self.event_builder.getValue())
-        
-        self.event_builder = EventBuilder(self.tomography_builder)
-        return self.event_builder
+    def Event(self, event = None):  
+        return self.tomography_builder.Event()
         
     def Hypocenter(self, hypocenter):
+        if(hypocenter != None):
+            self.hypocenter_list.append(hypocenter)
+            return self
+        
         self.hypocenter_builder = HypocenterBuilder(self)
         return self.hypocenter_builder
     
-    def Observation(self, observation):
-        self.observation_list.append(observation)
-        return self
+    def Observation(self, observation = None):
+        if(observation != None):
+            self.observation_list.append(observation)
+            return self
+        
+        if (self.observation_builder != None):
+            self.observation_list.append(self.observation_builder.getValue())
+            
+        self.observation_builder = ObservationBuilder(self)
+        return self.observation_builder
 
     def getValue(self):
-        if(self.event_builder != None):
-            self.event_list.append(self.event_builder.getValue())
         self.hypocenter_list.append(self.observation_list)
-        self.event_list.append(self.hypocenter_list)
         return self.hypocenter_list
     
     def execute(self):
+        if (self.hypocenter_builder != None):
+            self.hypocenter_list.append(self.hypocenter_builder.getValue())
+        
+        if (self.observation_builder != None):
+            self.observation_list.append(self.observation_builder.getValue())
+        
         self.tomography_builder.execute()
     
 class HypocenterBuilder(EventBuilder):
     hypocenter_list = []
+    event_builder = None
 
-    def __init__(self, EventBuilder):
-        self.EventBuilder = EventBuilder
+    def __init__(self, event_builder):
+        self.event_builder = event_builder
         
     def Location(self, location):
         self.location = location
@@ -76,11 +89,38 @@ class HypocenterBuilder(EventBuilder):
         
     def getValue(self):
         self.hypocenter_list.append([self.time, self.location])
+        
+    def execute(self):
+        self.event_builder.execute()
     
+class ObservationBuilder(EventBuilder):
+    observation_list = []
+    time = None
+    station = None
+    event_builder = None
     
+    def __init__(self, event_builder):
+        self.event_builder = event_builder
+        
+    def Observation(self, observation = None):
+        return self.event_builder.Observation()
     
+    def Station(self, station):
+        self.station = station
+        return self
+        
+    def Time(self, time):
+        self.time = time
+        return self
+        
+    def getValue(self):
+        self.hypocenter_list.append([self.station, self.time])
+        return self.hypocenter_list
+        
+    def execute(self):
+        self.event_builder.execute()
+
     
-    
-test = TomographyBuilder() \
-                            .Event('event') \
-                            .execute()
+test = TomographyBuilder().Event().Hypocenter("hypo")\
+                                    .Observation('obs1').Observation().Station('sta').Time('time') \
+                                    .execute()
